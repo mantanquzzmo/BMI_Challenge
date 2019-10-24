003b445dd5debec00db131510c12a9fa36bf74fc
mantanquzzmo <quzzmo@hotmail.com>
2019-10-24 15:34:21 -0400

    Phase 2 BMI Challenge

--- src/js/person.js
+++ src/js/person.js
@@ -1,8 +1,14 @@
 // src/js/person.js
 
+// src/js/person.js
+
 function Person(attr) {
     this.weight = attr.weight;
     this.height = attr.height;
+    this.calculate_bmi = function () {
+        calculator = new BMICalculator();
+        calculator.metric_bmi(this)
+    };
 };
 // src/js/person.js
 if (typeof module !== 'undefined' && module.exports) {
